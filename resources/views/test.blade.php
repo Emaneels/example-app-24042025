374260effe329f2bcdeaf3bf12aee29fd68108bf
unknown <bruno@example.com>
2025-04-24 14:43:18 +0300

    post model added

---
+++ resources/views/test.blade.php
@@ -0,0 +1,9 @@
+<?php
+    echo "<h1>test</h1>";
+    $name = "Emaneels";
+    echo $name, "<br />";
+
+    echo $variable;
+?>
+
+{{ $variable }}
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/test', function () {
+//     return view('welcome');
+// });
+
+Route::get('/test', function (){
+    return view('test', ['variable' => "my variable text value"]);
+});
+
+Route::get('/about', function (){
+    return view('about');
+});
+
+Route::get('/post', function (){
+    return view('post');
+});
